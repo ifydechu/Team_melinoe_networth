c9d8d380faf022769bb3a26b138b47a11b1dbf83
gfredtech <gfredtech@gmail.com>
2019-09-23 18:25:10 +0000

    heroku :(

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from "express";
 import bodyParse from "body-parser";
 import * as httpStatus from "http-status-codes";
 import Route from "./routes/route";
+import * as cors from "cors";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParse.json());
+app.use(cors());
 
 app.use("/api/v1", Route);
 
