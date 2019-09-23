92dfc0894cdf8c946eba1448f113bb41687dd2e4
horlard <oladipuposaulih@gmail.com>
2019-09-23 15:01:02 +0100

    update

---
+++ view/src/pages/signUp.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+class SignUp extends React.Component{
+    render(){
+        return (
+            <div>
+        <div
+          className="ui middle aligned center aligned grid"
+          style={{ transform: "translateY(30%)" }}
+        >
+          <div className="column">
+            <h2 className="ui teal image header">
+              <div className="content">Sign-up</div>
+            </h2>
+            <form className="ui large form error">
+              <div className="ui stacked segment">
+                
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="mail icon"></i>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="E-mail Address"
+                      onChange={this.inputHandler}
+                      required
+                    />
+                  </div>
+                </div>
+                <div className="field">
+                  <div className="ui left icon input">
+                    <i className="lock icon"></i>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      onChange={this.inputHandler}
+                      required
+                      minLength="6"
+                    />
+                  </div>
+                </div>
+                
+                <div>
+                  <button
+                    className="ui fluid large teal submit button"
+                  >
+                    Sign Up
+                  </button>
+                </div>
+              </div>
+              <div className="ui error message"></div>
+              <div className="ui error message"></div>
+              <div className="ui message">
+                Already have an account? <a href='#'>Login</a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+        )
+    }
+}
+export default SignUp;
