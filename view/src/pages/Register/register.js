ac2e6b70cbdfd9ae7bf2d3189565aa4a626d5221
horlard <oladipuposaulih@gmail.com>
2019-09-23 16:12:19 +0100

    register component

--- view/src/pages/Register/register.js
+++ view/src/pages/Register/register.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 class SignUp extends React.Component {
   render() {
@@ -49,7 +50,7 @@ class SignUp extends React.Component {
               <div className="ui error message"></div>
               <div className="ui error message"></div>
               <div className="ui message">
-                Already have an account? <a href="#">Login</a>
+                Already have an account? <Link to='/login'>Login</Link>
               </div>
             </form>
           </div>
